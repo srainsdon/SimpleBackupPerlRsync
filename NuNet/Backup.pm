241886de9082b83ac1a50ffcc69f9ecdaa9be306
Seth Rainsdon <srainsdon@nunetnetworks.net>
2015-02-24 07:39:48 -0800

    Saveing where I am Not much changed.

--- NuNet/Backup.pm
+++ NuNet/Backup.pm
@@ -7,10 +7,18 @@ use Term::ANSIColor;
 use File::Copy;
 use File::Path qw(make_path remove_tree);
 
-our $NumBk = 6; # Number of backups to keep
-our $DEBug = 0;
-our $BkDir = "/home/Media/Backups/";
-		 
+our $NumBk; # Number of backups to keep
+our $DEBug;	# Are we in Debug mode : does not move anything, delete anything or run the rsync command
+our $BkDir; # What directory are we backing up to
+our $SourceUser; # User name to use to connect to server
+our $SourceServer; # Where server are we rsyncing from
+our $SourceDir; # What directory is the source in
+our $RsyncOpt; # Extra rsync options
+
+sub new {
+	
+}
+
 sub MoveFiles {
 my $SiteBkDir = shift;
 my $SiteName = shift;
@@ -23,36 +31,18 @@ my $SiteName = shift;
 		print "move(\"$SiteBkDir/hourly.$Next\",\"$SiteBkDir/hourly.$CurrentBackup\")\n";
 		$CurrentBackup = $CurrentBackup - 1;
 	}	
-	
+	print "rsync $RsyncOpt --link-dest=$SiteBkDir/hourly.1/ $SourceUser\@$SourceServer:$SourceDir $SiteBkDir/hourly.0/";
 	#move("$SiteBkDir/hourly.2","$SiteBkDir/hourly.3");
 	#move("$SiteBkDir/hourly.1","$SiteBkDir/hourly.2");
 	#move("$SiteBkDir/hourly.0","$SiteBkDir/hourly.1");
 	
 	
-	#my $output = qx{rsync -h -z -e "ssh -i /home/srainsdon/.ssh/srv1" -a --force --delete-excluded --delete --stats --link-dest=$SiteBkDir/hourly.1/ srv1.247ly.com:/home/Hosting/Sites/$Site/ $SiteBkDir/hourly.0/};
+	#my $output = qx{rsync $RsyncOpt --link-dest=$SiteBkDir/hourly.1/ $SourceUser\@$SourceServer:$SourceDir $SiteBkDir/hourly.0/};
 	#warn("rsync failed?") if ($? / 256);
 	#my ($sent)     = ($output =~ /Total bytes sent: (\d+)/);
 	#my ($received) = ($output =~ /Total bytes received: (\d+)/);
 	#print "Totals: Sent $sent Received $received.\n";
 	print "DEBUG ON" if $DEBug = 1;
 	}
-foreach (@Sites) { 
-$Site = $_;
-$SiteBkDir = $BkDir . $Site;
-#print "Checking $SiteBkDir\n";
-if (-d $SiteBkDir) {
-    print color 'bold Green';
-    print "Found Directory for $Site\n";
-	print color 'reset';
-	MoveFiles($SiteBkDir, $Site);
-	
-} else {
-	#print "Missing Directory for $Site\n"
-	print color 'bold Red';
-	print "Missing Directory for $Site\n";
-	print color 'reset';
-	mkdir $SiteBkDir;
-}
-}
 
 1;
--- backup.pl
+++ backup.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/perl
 strict
 use NuNet::Backup
-
+$BkDir = "/home/Media/Backups/";
+$RsyncOpt = "-h -z -e \"ssh -i /home/srainsdon/.ssh/srv1\" -a --force --delete-excluded --delete --stats";
 @Sites = ("247ly.com",
          "perlcat.247ly.com",
 		 "slayer1of1.pirate",
